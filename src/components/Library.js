bbcc53abac47fafd9eafb25959ea46801e3e3672
mullum <mullum20@yandex.ru>
2025-02-20 22:28:53 +0500

    add: list map

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,8 @@
 import CardList from "./CardList";
 function Library() {
+    const cardLists = [1,2,3,4]
     return(
-        <div>
-            <CardList/>
-            <CardList/>
-        </div>
+        cardLists.map((list) => <CardList />)
     )
 }
 export default Library;
