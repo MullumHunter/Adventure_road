ecab8ae2fd490d3785d447e2ad1530861416e2d8
mullum <mullum20@yandex.ru>
2025-02-21 19:16:26 +0500

    add: CardData

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
 import './App.css';
 import Library from "./components/Library";
+import {library} from './components/CardData'
 
 function App() {
-    const library = [
-        {
-            id: 0,
-            name: "Первая",
-            items: 1,
-            skills: [
-                {
-                    run: 4,
-                    skale: 2
-                },
-                {
-                    flau: 2,
-                    skale: 3
-                }
-            ]
-        },
-        {
-            id: 1,
-            name: "Вторая",
-            items: 1,
-            skills: [
-                {
-                    run: 8,
-                    skale: 2
-                },
-                {
-                    flau: 9,
-                    skale: 3
-                }
-            ]
-        },
-    ];
-    
     return (
         <div className="App">
             <Library itemsLibrary={library} />
---
+++ src/components/CardData.js
@@ -0,0 +1,33 @@
+export const library = [
+    {
+        id: 0,
+        name: "Первая",
+        items: 1,
+        skills: [
+            {
+                run: 4,
+                skale: 2
+            },
+            {
+                flau: 2,
+                skale: 3
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: "Вторая",
+        items: 1,
+        skills: [
+            {
+                run: 8,
+                skale: 2
+            },
+            {
+                flau: 9,
+                skale: 3
+            }
+        ]
+    },
+];
+
