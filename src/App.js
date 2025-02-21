8cf8be2ccebb9c2eee87691997af5f1e466a4702
mullum <mullum20@yandex.ru>
2025-02-21 14:38:18 +0500

    add: btn raw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
 import Library from "./components/Library";
+import {useState} from "react";
 
 function App() {
     const library = [
         {
-        id: 0,
-        name: "Первая",
-        items: 1
-        },
-        {
-            id: 1,
-            name: "Вторая",
-            items: 2
+            id: 0,
+            name: "Первая",
+            items: 1
         },
     ]
-
+    const [lib, setLib] = useState(library)
+    const changeLib = () => {
+        const newCard = {id: lib.length, name: `Библиотека ${lib.length + 1}`, items: 1};
+        setLib([...lib, newCard]);
+    }
+    console.log(lib)
     return(
         <div className="App">
-            <Library itemsLibrary={library}/>
+            <Library itemsLibrary={lib}/>
+            <button onClick={changeLib}>Add</button>
         </div>
     )
 }
