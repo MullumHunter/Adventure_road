944d5ff3e9b10799db51f0e2bf0a4dd53cf77269
mullum <mullum20@yandex.ru>
2025-02-24 21:33:25 +0500

    add: cardsData add cardCatalog

--- src/components/data/ListData.js
+++ src/components/data/ListData.js
@@ -1,32 +1,18 @@
+import {cardCatalog} from "./CardCatalog";
 export const cardsData = [
     {
         id: 0,
         nameLibrary: "Первая",
-        items: 1,
-        skills: [
-            {
-                run: 4,
-                skale: 2
-            },
-            {
-                flau: 2,
-                skale: 3
-            }
+        cards: [
+            cardCatalog[0],
+            cardCatalog[1],
         ]
     },
     {
         id: 1,
         nameLibrary: "Вторая",
-        items: 1,
-        skills: [
-            {
-                run: 8,
-                skale: 2
-            },
-            {
-                flau: 9,
-                skale: 3
-            }
+        cards: [
+            cardCatalog[2],
         ]
     },
 ];
