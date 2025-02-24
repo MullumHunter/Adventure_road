84089ed67568ff6ba8c1d6cc276846c11d0c1e43
mullum <mullum20@yandex.ru>
2025-02-24 19:03:55 +0500

    add: cardCatalog id

--- src/components/data/CardCatalog.js
+++ src/components/data/CardCatalog.js
@@ -1,6 +1,7 @@
 import {characteristicsData} from "./characteristicsData";
 export  const cardCatalog =[
     {
+        id: 0,
         img: '../styles/imj/cards/3.jpg',
         skills:[
             characteristicsData[0],
@@ -10,6 +11,7 @@ export  const cardCatalog =[
         title:'Не перед чем не отступится но получит желаемое или умрет'
     },
     {
+        id: 1,
         img: '../styles/imj/cards/2.jpg',
         skills:[
             characteristicsData[1],
@@ -19,6 +21,7 @@ export  const cardCatalog =[
         title:'Берется за любое дело что бы узнать что будет'
     },
     {
+        id: 2,
         img: '../styles/imj/cards/1.jpg',
         skills:[
             characteristicsData[5],
