29aca8869da9752052f058192b83ffbf49a8ae99
mullum <mullum20@yandex.ru>
2025-02-25 21:04:04 +0500

    add: add Challenge

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,17 @@
 import styles from "./styles/Card.module.css";
-import cardCharacteristics from './styles/CardCharacteristics.module.css'
 import Way from "./Way";
+import Challenge from "./Challenge";
 
 function Card({id, nameCard, hard, img, ways, requirement,act,author,addition,cardNumber,year }) {
     console.log(ways[0].nameWayUp)
     console.log(ways[0].nameWayDown)
     return (
         <div className={styles.Card} style={{ backgroundImage: `url(${img})`}}>
-            {/*<div className={cardCharacteristics.Way}>*/}
-                
-                    <Way title={ways[0].nameWayUp}/>
-                    <Way title={ways[1].nameWayDown}/>
-                
-            {/*</div>*/}
+            
+            <Way title={ways[0].nameWayUp}/>
+            <Challenge title={nameCard}/>
+            <Way title={ways[1].nameWayDown}/>
+            
         </div>
     );
 }
---
+++ src/components/Challenge.js
@@ -0,0 +1,5 @@
+import styles from './styles/Challenge.module.css'
+function Challenge({title}) {
+    return <div className={styles.Challenge}>{title}</div>
+}
+export default Challenge;
