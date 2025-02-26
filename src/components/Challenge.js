734600b762765259a86fa59769eb322832a3ca5e
mullum <mullum20@yandex.ru>
2025-02-26 16:53:32 +0500

    add: chain correction

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -1,21 +1,11 @@
 import styles from './styles/Challenge.module.css'
-import Hard from "./Hard";
-import Rune from "./Rune";
-import Chain from "./Chain";
+import Requirements from "./requirements";
 function Challenge({title}) {
     return(
         <>
             
             <div className={styles.Chain}>
-                <div className={styles.Rune}>
-                    <div className={styles.Hard}>
-                        <Hard/>
-                    </div>
-                    <Chain/>
-                    <Rune/>
-                    <Chain/>
-                    <Rune/>
-                </div>
+                <Requirements/>
                 <div className={styles.Challenge}>
                     Danke schön
                 </div>
---
+++ src/components/requirements.js
@@ -0,0 +1,18 @@
+import Hard from "./Hard";
+import Chain from "./Chain";
+import Rune from "./Rune";
+
+function Requirements() {
+    return (
+        <>
+            <Hard/>
+            <Chain/>
+            
+            <Chain/>
+            
+            <Rune/>
+            
+        </>
+    )
+}
+export default Requirements;
