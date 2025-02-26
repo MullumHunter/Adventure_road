6fcd0eca6ee03a3ce37f2119f417ac23ea3d7d1a
mullum <mullum20@yandex.ru>
2025-02-26 16:54:17 +0500

    add: Requirements

--- src/components/requirements.js
+++ src/components/requirements.js
@@ -1,15 +1,12 @@
 import Hard from "./Hard";
 import Chain from "./Chain";
 import Rune from "./Rune";
-
 function Requirements() {
     return (
         <>
             <Hard/>
             <Chain/>
-            
             <Chain/>
-            
             <Rune/>
             
         </>
