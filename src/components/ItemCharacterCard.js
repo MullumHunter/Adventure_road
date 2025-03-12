c3654649d16fa0dc655ac9355a0463bb93b52d63
mullum <mullum20@yandex.ru>
2025-03-12 22:14:25 +0500

    add: img for haracter

--- src/components/ItemCharacterCard.js
+++ src/components/ItemCharacterCard.js
@@ -3,13 +3,13 @@ import styles from './styles/ItemCharacterCard.module.css';
 const images = {
     dark_way: "/img/triumph_points/dark_way.png",
     experience: "/img/itemCharacterCard/experience.png",
+    card_hero: "/img/triumph_points/hero_card.png",
     month: "/img/itemCharacterCard/moon.png",
-    intelligence: "/img/runes/intellect.png",
     sun: "/img/runes/wisdom.png",
+    intelligence: "/img/runes/intellect.png",
     wisdom: "/img/triumph_points/sun.png",
     strength: "/img/runes/strength.png",
     constitution: "/img/runes/constitution.png",
-    card_hero: "/img/triumph_points/hero_card.png",
 };
 
 function ItemCharacterCard({ type }) {
--- src/components/TextCharacterCard.js
+++ src/components/TextCharacterCard.js
@@ -1,49 +1,30 @@
 import ItemCharacterCard from "./ItemCharacterCard";
-import styles from './styles/ItemCharacterCard.module.css';
+import styles from "./styles/ItemCharacterCard.module.css";
 
 function TextCharacterCard({ text }) {
-    const parseText = (text) => {
-        const parts = text.split(/(\(experience\)|\(strength\)|\(dark_way\)|\(constitution\)|\(intelligence\)|\(wisdom\)|\(sun\)|\(card_hero\)|\(month\)|\n)/g);
-        
-        return parts.map((part, index) => {
-            if (part === "(experience)") {
-                return <ItemCharacterCard key={index} type="experience" />;
-            }
-            if (part === "(month)") {
-                return <ItemCharacterCard key={index} type="month" />;
-            }
-            if (part === "(dark_way)") {
-                return <ItemCharacterCard key={index} type="dark_way" />;
-            }
-            if (part === "(card_hero)") {
-                return <ItemCharacterCard key={index} type="card_hero" />;
-            }
-            if (part === "(sun)") {
-                return <ItemCharacterCard key={index} type="sun" />;
-            }
-            if (part === "(wisdom)") {
-                return <ItemCharacterCard key={index} type="wisdom" />;
-            }
-            if (part === "(constitution)") {
-                return <ItemCharacterCard key={index} type="constitution" />;
-            }
-            if (part === "(strength)") {
-                return <ItemCharacterCard key={index} type="strength" />;
-            }
-            if (part === "(intelligence)") {
-                return <ItemCharacterCard key={index} type="intelligence" />;
-            }
-            if (part === "\n") {
-                return <br key={index} />;
-            }
-            return part;
-        });
+    const emojiMap = {
+        "(experience)": "experience",
+        "(month)": "month",
+        "(dark_way)": "dark_way",
+        "(card_hero)": "card_hero",
+        "(sun)": "sun",
+        "(wisdom)": "wisdom",
+        "(constitution)": "constitution",
+        "(strength)": "strength",
+        "(intelligence)": "intelligence",
     };
-    return (
-        <div className={styles.Box}>
-            {parseText(text)}
-        </div>
-    );
+    
+    const parseText = (text) => {
+        return text.split(/(\(experience\)|\(strength\)|\(dark_way\)|\(constitution\)|\(intelligence\)|\(wisdom\)|\(sun\)|\(card_hero\)|\(month\)|\n)/g)
+            .map((part, index) => {
+                if (emojiMap[part]) {
+                    return <ItemCharacterCard key={index} type={emojiMap[part]} />;
+                }
+                return part === "\n" ? <br key={index} /> : part;
+            });
+    };
+    
+    return <div className={styles.Box}>{parseText(text)}</div>;
 }
 
 export default TextCharacterCard;
