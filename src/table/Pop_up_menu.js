b60b52db612a141c6800eeff9a2398f3298ab1c5
mullum <mullum20@yandex.ru>
2025-03-30 08:40:04 +0500

    add: selected

--- src/table/Pop_up_menu.js
+++ src/table/Pop_up_menu.js
@@ -1,12 +1,17 @@
 import styles from "../table/styleTable/Pop_up_menu.module.css"
+import {useState} from "react";
 function Pop_up_menu({list}) {
+    const [selectedLibrary, setSelectedLibrary] = useState(null);
+    
+    const handleSelect = (library) => { setSelectedLibrary(library) };
     
     return(
         <div>
             <h3>Названия библиотек:</h3>
             <div className={styles.Container}>
                 {Object.keys(list).map((name, index) => (
-                    <div className={styles.Item} key={index}>
+                    <div className={`${styles.Item} ${selectedLibrary === name? styles.selected : ``}`}
+                         key={name} onClick={() => handleSelect(name)}>
                         {name}
                     </div>
                 ))}
