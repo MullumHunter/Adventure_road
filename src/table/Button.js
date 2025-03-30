1bc0d900099614afcd1d8a909488278bd050dfc5
mullum <mullum20@yandex.ru>
2025-03-30 19:07:36 +0500

    add: btn savedState

--- src/table/Button.js
+++ src/table/Button.js
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./styleTable/Button.module.css";
 
 function Button({ text, onClick }) {
-    const [isActive, setIsActive] = useState(false);
+    // Восстановление состояния кнопки из localStorage или по умолчанию false
+    const [isActive, setIsActive] = useState(() => {
+        const savedState = localStorage.getItem("isButtonActive");
+        return savedState ? JSON.parse(savedState) : false;
+    });
+    
+    // Сохранение состояния кнопки в localStorage
+    useEffect(() => {
+        localStorage.setItem("isButtonActive", JSON.stringify(isActive));
+    }, [isActive]);
     
     const handleClick = () => {
-        setIsActive((prev) => !prev); // Переключение состояния активной кнопки
+        setIsActive((prev) => !prev); // Переключение состояния кнопки
         if (onClick) onClick(); // Вызов callback для дальнейшей обработки
     };
     
