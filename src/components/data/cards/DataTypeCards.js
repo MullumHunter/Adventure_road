0a50f27d7c75ca0703ffb1e1a91bd40e6f1a080a
mullum <mullum20@yandex.ru>
2025-03-31 21:23:12 +0500

    add: SelectWrapper css

--- src/components/data/cards/DataTypeCards.js
+++ src/components/data/cards/DataTypeCards.js
@@ -1,1 +1,9 @@
-export const dataTypeCards = ["challenge", "character", "ally", "villian"];
+export const dataTypeCards = {
+    "challenge": "Карты испытаний"
+    ,
+    "character": "Карты черт характера"
+    ,
+    "ally": "Карты союзников"
+    ,
+    "villian": "Карты испытания Злодеи"
+};
--- src/table/FilterTypeCards.js
+++ src/table/FilterTypeCards.js
@@ -1,14 +1,31 @@
-function FilterTypeCards({typeCards}) {
-    console.log(typeCards)
-    return(
-        <select>
-            {typeCards.map((item, index)=>(
-                <option key={index} value={item}>
-                    {item}
-                </option>
-                )
+import React, { useState } from 'react';
+import styles from '../table/styleTable/FilterTypeCards.module.css';
+
+function FilterTypeCards({ typeCards }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState('');
+    
+    const handleSelect = (key) => {
+        setSelectedOption(typeCards[key]);
+        setIsOpen(false);
+    };
+    
+    return (
+        <div className={styles.SelectWrapper}>
+            <button className={styles.SelectButton} onClick={() => setIsOpen(!isOpen)}>
+                {selectedOption || 'Выберите тип карт'}
+            </button>
+            {isOpen && (
+                <ul className={styles.OptionsList}>
+                    {Object.entries(typeCards).map(([key, value], index) => (
+                        <li key={index} className={styles.OptionItem} onClick={() => handleSelect(key)}>
+                            {value}
+                        </li>
+                    ))}
+                </ul>
             )}
-        </select>
-    )
+        </div>
+    );
 }
+
 export default FilterTypeCards;
